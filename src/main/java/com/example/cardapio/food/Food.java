7362c32659ecbd0b8f3c21083bf0cd6d704e07c6
pedroargioni <pedroargioni@gmail.com>
2023-11-27 14:09:59 -0300

    first commit

---
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,19 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+}
